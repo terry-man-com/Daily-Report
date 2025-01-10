0c2b68f030481bc97880a8aa2c5f05b7c00bbd25
terry-man-com <terry.man.com@icloud.com>
2025-01-11 00:30:16 +0900

    add main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
 // 設定情報
 const firebaseConfig = {
@@ -18,7 +18,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Cloud Firestoreから取得したデータを表示する
-const fetchHistoryDate = async () => {
+const fetchHistoryData = async () => {
     let tags = "";
 
     //reportsコレクションからデータを取得
@@ -27,17 +27,31 @@ const fetchHistoryDate = async () => {
     // データをテーブル表の形式に合わせてHTMLに挿入
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${doc.data()}`);
-      tags += `<tr>
-                <td>${doc.data().date}</td>
-                <td>${doc.data().name}</td>
-                <td>${doc.data().work}</td>
-                <td>${doc.data().comment}</td>
-            </tr>`;
+      tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`;
     });
     document.getElementById("js-history").innerHTML = tags;
 };
 
 // Cloud Firestoreから取得したデータを表示する
 if(document.getElementById("js-history")) {
-  fetchHistoryDate();
+  fetchHistoryData();
+}
+
+// Cloud Firestoreにデータを送信する
+const submitData = async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  try {
+    const docRef = await addDoc(collection(db, "reports"), {
+      data: new Date(),
+      name: formData.get("name"),
+      work: formData.get("work"),
+      comment: formData.get("comment")
+    });
+    console.log("Document written with ID: ", docRef,id);
+  } catch (e) {
+    console.log("Error adding document: ", e);
+  }
 }
