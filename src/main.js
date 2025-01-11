9436949dafe6bf15617f4299f555495060768135
terry-man-com <terry.man.com@icloud.com>
2025-01-12 00:38:32 +0900

    add main.js

--- src/main.js
+++ src/main.js
@@ -42,16 +42,19 @@ const submitData = async (e) => {
   e.preventDefault();
 
   const formData = new FormData(e.target);
-
   try {
     const docRef = await addDoc(collection(db, "reports"), {
-      data: new Date(),
+      date: new Date(),
       name: formData.get("name"),
       work: formData.get("work"),
       comment: formData.get("comment")
     });
-    console.log("Document written with ID: ", docRef,id);
+    console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.log("Error adding document: ", e);
   }
 }
+// submitボタンを押下し、Cloud Firestoreにデータ送信した時の処理
+if(document.getElementById("js-form")) {
+  document.getElementById("js-form").addEventListener("submit", (e) => submitData(e));
+};
